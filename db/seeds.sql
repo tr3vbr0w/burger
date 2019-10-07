6c2abdf68c72490f144cae24a4ad002935c9e314
tbsteeze <trevbrow@live.com>
2019-10-06 21:09:10 -0600

    started setting up db, yet to go into the command information

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger) VALUES ('Chili Burger');
+INSERT INTO burgers (burger) VALUES ('Bacon BBQ Burger');
+INSERT INTO burgers (burger, devoured) VALUES ('Firecracker', true);
+INSERT INTO burgers (burger, devoured) VALUES ('Bacon Bleu', true);
+INSERT INTO burgers (burger, devoured) VALUES ('Joe Biden', true);
+INSERT INTO burgers (burger) VALUES ('Cowboy Burger');
